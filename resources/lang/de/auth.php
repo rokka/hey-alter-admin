1085c646d1b375a0b00bf0d338a055befbddd8f5
Andre Scheidtmann <rokka@gmx.net>
2021-04-06 17:42:47 +0200

    Fitst german localization

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Diese Anmeldeinformationen stimmen nicht mit unseren Aufzeichnungen überein.',
+    'password' => 'Das angegebene Passwort ist nicht korrekt.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuchen Sie es erneut in :seconds Sekunden.',
+
+];
---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'Dein Passwort wurde zurück gesetzt!',
+    'sent' => 'Wir haben Ihren Link zum Zurücksetzen Ihres Passworts per E-Mail gesendet!',
+    'throttled' => 'Bitte warten Sie, bevor Sie es erneut versuchen.',
+    'token' => 'Dieses Token zum Zurücksetzen des Passworts ist ungültig.',
+    'user' => "Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.",
+
+];
