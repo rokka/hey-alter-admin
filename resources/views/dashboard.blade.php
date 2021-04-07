f4ae46512057fb614afc9f97b3097f14b2b5c14e
Andre Scheidtmann <rokka@gmx.net>
2021-04-07 12:12:08 +0200

    Remove dashboard text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,10 +18,6 @@
     </div>
 
     <div class="mt-6 text-gray-500">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
     </div>
 </div>
 
