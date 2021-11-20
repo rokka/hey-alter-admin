171465234ae4bb04786a99f95cc70370911d9d49
Andre Scheidtmann <scheidtmann@mauve.de>
2021-11-20 19:13:52 +0100

    Einlieferungsliste ausblenden und Darstellung für Smartphones angepasst

--- resources/views/computers/show.blade.php
+++ resources/views/computers/show.blade.php
@@ -8,7 +8,7 @@
     <div>
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8">
-                <a href="{{ route('computers.edit', $computer->id) }}" class="inline-flex items-center justify-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-600 disabled:opacity-25 transition inline-block">Computer bearbeiten</a>
+                <a href="{{ route('computers.edit', $computer->id) }}" class="inline-flex items-center justify-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-600 disabled:opacity-25 transition inline-block">Bearbeiten</a>
                 <div class="float-right inline-block">
                     @livewire('computers.delete', ['computer' => $computer])
                 </div>
@@ -18,14 +18,13 @@
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200 w-full table-fixed">
-
                                 <thead class="bg-gray-50">
                                     <tr>
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/3">Hardware</th>
                                         <th class="w-2/3"></th>
                                     </tr>
                                 </thead>
-
+                                
                                 @if (Auth::user()->currentTeam->use_donor_information)
                                 <tr class="border-b">
                                     <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -207,7 +206,6 @@
                     </div>
                 </div>
             </div>
-
 
             <div class="flex flex-col mt-4">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
