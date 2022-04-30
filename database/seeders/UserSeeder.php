06f46cabc02cf3e6458ffe01a1441a52416432b0
Andre <rokka@gmx.net>
2022-04-30 10:35:40 +0200

    Fix üfr User Seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'id' => 1,
             'name' => 'Test Benutzer',
-            'email' => 'test@test.de',
+            'email' => 'test@heyalter.com',
             'password' => '$2y$10$cQXjmHIFw1.9sYrYlv.3DeU6EiFG95XqGzl7g735L8xQFvKq7YGaa', //testtest
             'current_team_id' => 1,
         ]);
